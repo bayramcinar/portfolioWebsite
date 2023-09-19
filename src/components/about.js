7cf78d07c4122a8b04cb433ec9790cafebf5ad02
Bayram Çınar <99193151+bayramcinar@users.noreply.github.com>
2023-09-19 19:31:32 +0300

    about.js

--- src/components/about.js
+++ src/components/about.js
@@ -22,23 +22,23 @@ useEffect(()=>{
       <div className='aboutDiv row'>
         <img className='gif2 col-lg-6 col-sm-12' src={gif3}></img>
         <div className='aboutmeArea col-lg-6 col-sm-12'>
-            <h3 data-aos="zoom-in" className='aboutmeTitle'>Bayram Çınar</h3>
-            <h4 data-aos="zoom-in" className='aboutmeJob'>Junior Software Developer</h4>
-            <p data-aos="zoom-in" className='aboutmeText'>I am studying Computer Engineering at Cukurova University (3rd
+            <h3 data-aos="fade-down" className='aboutmeTitle'>Bayram Çınar</h3>
+            <h4 data-aos="fade-down" className='aboutmeJob'>Junior Software Developer</h4>
+            <p data-aos="fade-down" className='aboutmeText'>I am studying Computer Engineering at Cukurova University (3rd
             grade). I am 22 years old. I am a junior full stack web developer.
             I know HTML, CSS, JavaScript, ReactJs, NodeJS, Pyhton and
             Typescript. I am currently learing Angular and .Net Core.
             </p>
-            <h6 data-aos="zoom-in">
+            <h6 data-aos="fade-down">
             🥷I'm currently working on Web Developing
             </h6>
-            <h6 data-aos="zoom-in">
+            <h6 data-aos="fade-down">
             💥I'm learning Angular and .NET Core
             </h6>
-            <h6 data-aos="zoom-in">
+            <h6 data-aos="fade-down">
             ⚡Ask me about Web Developing
             </h6>
-            <h6 data-aos="zoom-in">
+            <h6 data-aos="fade-down">
             📩E-mail: bayram200212@gmail.com
             </h6>
         </div>
